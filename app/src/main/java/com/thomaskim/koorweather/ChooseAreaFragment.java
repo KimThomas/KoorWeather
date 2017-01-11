b4f9c769d863af4dac832609e6bb00d80712ad4b
Thomas <junie478@gmail.com>
2017-01-11 20:12:11 +0800

    get province,city,county

---
+++ app/src/main/java/com/thomaskim/koorweather/ChooseAreaFragment.java
@@ -0,0 +1,246 @@
+package com.thomaskim.koorweather;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.thomaskim.koorweather.db.City;
+import com.thomaskim.koorweather.db.County;
+import com.thomaskim.koorweather.db.Province;
+import com.thomaskim.koorweather.util.HttpUtil;
+import com.thomaskim.koorweather.util.JsonUtil;
+
+import org.litepal.crud.DataSupport;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+/**
+ * Created by ThomasKim on 2017/1/11.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+
+    public static final int LEVEL_PROVINCE=0;
+    public static final int LEVEL_CITY=1;
+    public static final int LEVEL_COUNTY=2;
+
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> dataList=new ArrayList<>(); //临时存放省，市等数据的集合
+
+    private List<Province> provinceList;//全部的省的集合
+    private List<City> cityList; //省里面市的集合
+    private List<County> countyList; //市里面县的集合
+
+    private Province selectedProvince; //当前选中的省
+    private City selectedCity;  //当权选中的城市
+    private int currentLevel;  //当前选中的级别
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //初始化各个空间
+        View view=inflater.inflate(R.layout.choose_area,container,false);
+        titleText= (TextView) view.findViewById(R.id.title_text);
+        backButton= (Button) view.findViewById(R.id.back_button);
+        listView= (ListView) view.findViewById(R.id.list_view);
+        adapter=new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //设置ListView里面每个项目的点击事件，除了县城之外，其余的每点击就要往下一层查询
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(currentLevel==LEVEL_PROVINCE){
+                    selectedProvince=provinceList.get(position);
+                    queryCities();
+                }else if(currentLevel==LEVEL_CITY){
+                    selectedCity=cityList.get(position);
+                    queryCounties();
+                }
+            }
+        });
+        //返回按钮点击事件，除了省，其余的每点击一次则往上一层查询
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentLevel==LEVEL_COUNTY){
+                    queryCities();
+                }else if(currentLevel==LEVEL_CITY){
+                    queryProvinces();
+                }
+            }
+        });
+        queryProvinces();
+    }
+
+    /**
+     * 查询全国所有的省，先从数据库查询，查询不到再去服务器上下载JSON格式的数据
+     *
+     */
+    private void queryProvinces(){
+        titleText.setText("中国");
+        backButton.setVisibility(View.GONE);
+        provinceList= DataSupport.findAll(Province.class);
+        if(provinceList.size()>0){
+            dataList.clear();
+            for(Province province:provinceList){
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=LEVEL_PROVINCE;
+        }else{
+            String address="http://guolin.tech/api/china";
+            queryFromService(address,"province");
+        }
+    }
+
+    /**
+     * 查询选中省内的所有城市,查询方式如上面一样
+     */
+    private void queryCities(){
+        titleText.setText(selectedProvince.getProvinceName());
+        backButton.setVisibility(View.VISIBLE);
+        cityList=DataSupport.where("provinceid=?",String.valueOf(selectedProvince.getId())).find(City.class);
+        if(cityList.size()>0){
+            dataList.clear();
+            for(City city:cityList){
+                dataList.add(city.getCityName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=LEVEL_CITY;
+        }else{
+            int provinceCode=selectedProvince.getProvinceCode();
+            String address="http://guolin.tech/api/china/"+provinceCode;
+            queryFromService(address,"city");
+        }
+    }
+
+    /**
+     * 查询所选中市里面的县城，查询方法上同
+     */
+    private void queryCounties(){
+        titleText.setText(selectedCity.getCityName());
+        backButton.setVisibility(View.VISIBLE);
+        countyList=DataSupport.where("cityid=?",String.valueOf(selectedCity.getId())).find(County.class);
+        if(countyList.size()>0){
+            dataList.clear();
+            for(County county:countyList){
+                dataList.add(county.getCountyName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=LEVEL_COUNTY;
+        }else{
+            int provinceCode=selectedProvince.getProvinceCode();
+            int cityCode=selectedCity.getCityCode();
+            String address="http://guolin.tech/api/china/"+provinceCode+"/"+cityCode;
+            queryFromService(address,"county");
+        }
+    }
+
+    /**
+     * 根据传入的地址来进行省市县数据的下载
+     */
+    private void queryFromService(String address, final String type){
+        showProgressDialog();
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            /**
+             * 如果失败了，则切换成主线程去提示用户失败了
+             * @param call
+             * @param e
+             */
+            @Override
+            public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(), "加载失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            /**
+             * 把接受到的数据存放到数据库
+             * 并且查询数据把数据取出来显示再ListView里面
+             * @param call
+             * @param response
+             * @throws IOException
+             */
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText=response.body().string();
+                boolean result=false;
+                if("province".equals(type)){
+                    result= JsonUtil.handleProvinceResponse(responseText);
+                }else if("city".equals(type)){
+                    result=JsonUtil.handleCityResponse(responseText,selectedProvince.getId());
+                }else if("county".equals(type)){
+                    result=JsonUtil.handleCountyResponse(responseText,selectedCity.getId());
+                }
+                if(result){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if("province".equals(type)){
+                                queryProvinces();
+                            }else if("city".equals(type)){
+                                queryCities();
+                            }else if("county".equals(type)){
+                                queryCounties();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    /**
+     * 显示进度对话框
+     */
+    private void showProgressDialog(){
+        if(progressDialog==null){
+            progressDialog=new ProgressDialog(getActivity());
+            progressDialog.setMessage("正在加载。。。。");
+            progressDialog.setCanceledOnTouchOutside(false);
+        }
+        progressDialog.show();
+    }
+    /**
+     * 关闭进度对话框
+     */
+    private void closeProgressDialog(){
+        if(progressDialog!=null){
+            progressDialog.dismiss();
+        }
+    }
+}
---
+++ app/src/main/java/com/thomaskim/koorweather/util/JsonUtil.java
@@ -0,0 +1,89 @@
+package com.thomaskim.koorweather.util;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.thomaskim.koorweather.db.City;
+import com.thomaskim.koorweather.db.County;
+import com.thomaskim.koorweather.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by ThomasKim on 2017/1/11.
+ */
+
+public class JsonUtil {
+
+    public static final String JUTAG="JUTAG";
+
+    /**
+     * 解析和处理服务器返回的全部省的JSON格式数据
+     */
+    public static boolean handleProvinceResponse(String response){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allProvinces=new JSONArray(response);
+                for(int i=0;i<allProvinces.length();i++){
+                    JSONObject object=allProvinces.getJSONObject(i);
+                    Province province=new Province();
+                    province.setProvinceName(object.getString("name"));
+                    province.setProvinceCode(object.getInt("id"));
+                    province.save();
+                }
+                return true;
+            }catch (JSONException e){
+                LogUtil.d("province",e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解析和处理服务器返回的市的JSON格式的数据
+     */
+    public static boolean handleCityResponse(String response,int provinceId){
+        if(!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCities=new JSONArray(response);
+                for(int i=0;i<allCities.length();i++){
+                    JSONObject object=allCities.getJSONObject(i);
+                    City city=new City();
+                    city.setCityCode(object.getInt("id"));
+                    city.setCityName(object.getString("name"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                LogUtil.d("city",e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解析和处理服务器返回的县的JSON格式的数据
+     */
+    public static boolean handleCountyResponse(String response,int cityId){
+        if(!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCounties=new JSONArray(response);
+                for(int i=0;i<allCounties.length();i++){
+                    JSONObject object=allCounties.getJSONObject(i);
+                    County county=new County();
+                    county.setCountyName(object.getString("name"));
+                    county.setCityId(cityId);
+                    county.setWeatherId(object.getString("weather_id"));
+                    county.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                LogUtil.d("county",e.getMessage());
+            }
+        }
+        return false;
+    }
+}
